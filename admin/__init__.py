781c024e05a69d90e23ab61cb64cbaa3dec8cf1d
cheeleong <klrkdekira@gmail.com>
2013-09-20 15:14:25 +0800

    Initial commit

---
+++ admin/__init__.py
@@ -0,0 +1,24 @@
+from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
+
+from .models import (
+    DBSession,
+    Base,
+    )
+
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.bind = engine
+    config = Configurator(settings=settings)
+
+    config.include('pyramid_formalchemy')
+    config.include('fa.jquery')
+
+    config.formalchemy_admin('admin', package='pyramidapp',
+                             view='fa.jquery.pyramid.ModelView')
+
+    return config.make_wsgi_app()
---
+++ setup.py
@@ -0,0 +1,50 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'pyramid_jinja2',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+    'pyramid_formalchemy',
+    'pyramid_fanstatic',
+    'fa.jquery',
+    'gevent',
+    'gunicorn',
+    ]
+
+setup(name='admin',
+      version='0.0',
+      description='admin',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='admin',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = admin:main
+      [console_scripts]
+      initialize_admin_db = admin.scripts.initializedb:main
+      """,
+      )
